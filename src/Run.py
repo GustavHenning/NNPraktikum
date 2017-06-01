b3d5454c2c8377aef3e9becbef6db54bd724a847
Gustav <nightmunnas@gmail.com>
2017-06-01 14:27:51 +0200

    added skeleton for exercise 2

--- src/Run.py
+++ src/Run.py
@@ -4,6 +4,7 @@
 from data.mnist_seven import MNISTSeven
 from model.stupid_recognizer import StupidRecognizer
 from model.perceptron import Perceptron
+from model.logistic_regression import LogisticRegression
 from report.evaluator import Evaluator
 
 
@@ -17,7 +18,7 @@ def main():
                                         data.testSet,
                                         learningRate=0.005,
                                         epochs=30)
-    
+
     # Train the classifiers
     print("=========================")
     print("Training..")
@@ -34,7 +35,7 @@ def main():
     # Explicitly specify the test set to be evaluated
     stupidPred = myStupidClassifier.evaluate()
     perceptronPred = myPerceptronClassifier.evaluate()
-    
+
     # Report the result
     print("=========================")
     evaluator = Evaluator()
@@ -46,7 +47,7 @@ def main():
     print("\nResult of the Perceptron recognizer:")
     # evaluator.printComparison(data.testSet, perceptronPred)
     evaluator.printAccuracy(data.testSet, perceptronPred)
-    
+
 
 if __name__ == '__main__':
     main()
